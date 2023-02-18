703be58880eac953565b9d2b7b38aa44b97e82f5
JayDampitan <jaykennethpd@gmail.com>
2023-02-18 08:34:21 -0800

    install and apply framer motion animation on the mobile navbar when entering and exciting.

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -8,10 +8,29 @@ import {
   AiOutlineInstagram,
 } from "react-icons/ai";
 import { GrFacebookOption, GrInstagram } from "react-icons/gr";
+import { motion, AnimatePresence } from "framer-motion";
 
 type Props = {
   translate: boolean;
   setTranslate: any;
+};
+
+const mobileVariant = {
+  hidden: {
+    x: "100%",
+  },
+  exit: {
+    x: "100%",
+    transition: {
+      duration: 0.5,
+    },
+  },
+  visible: {
+    x: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
 };
 
 const Navbar = ({ translate, setTranslate }: Props) => {
@@ -28,60 +47,65 @@ const Navbar = ({ translate, setTranslate }: Props) => {
   return (
     <nav className="bg-regal-blue">
       {/* ----------------Mobile Menu------------------ */}
-      <div
-        className={` ${
-          navbarOpen ? "flex" : "hidden"
-        } bg-regal-blue flex-col fixed w-full bottom-0 top-0 right-0 left-0 sm:hidden text-off-white h-screen flex justify-center duration-500
-        `}
-      >
-        <section className="h-4/5 py-10">
-          <div
-            onClick={handleNavbar}
-            className="text-center w-screen items-center px-20 flex justify-end "
+      <AnimatePresence>
+        {navbarOpen && (
+          <motion.div
+            variants={mobileVariant}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            className="flex bg-regal-blue flex-col fixed w-full bottom-0 top-0 right-0 left-0 sm:hidden text-off-white h-screen justify-center"
           >
-            <AiOutlineClose className="text-3xl" />
-          </div>
-
-          <div
-            onClick={handleNavbar}
-            className="text-center text-3xl h-1/6 flex justify-center items-center"
-          >
-            <Link href="/">
-              <div>LOGO</div>
-            </Link>
-          </div>
-
-          <div>
-            <ul className="text-center text-3xl uppercase">
-              {NAV__LINKS.map((item, index) => (
-                <li
-                  onClick={handleNavbar}
-                  key={index}
-                  className="px-8 text-off-white my-3"
-                >
-                  <Link href={`${item.path}`}>
-                    {translate ? item.spanish : item.display}
-                  </Link>
-                </li>
-              ))}
+            <section className="h-4/5 py-10">
               <div
-                onClick={() => {
-                  setNavbarOpen(!navbarOpen);
-                  setTranslate(!translate);
-                }}
-                className="my-3 py-2"
+                onClick={handleNavbar}
+                className="text-center w-screen items-center px-20 flex justify-end "
               >
-                {translate ? "english" : "español"}
+                <AiOutlineClose className="text-3xl" />
               </div>
-            </ul>
-          </div>
 
-          <div className="flex justify-center my-8 text-3xl text-off-white">
-            <GrInstagram className="mx-2" />
-            <GrFacebookOption className="mx-2" />
-          </div>
-        </section>
-      </div>
+              <div
+                onClick={handleNavbar}
+                className="text-center text-3xl h-1/6 flex justify-center items-center"
+              >
+                <Link href="/">
+                  <div>LOGO</div>
+                </Link>
+              </div>
+
+              <div>
+                <ul className="text-center text-3xl uppercase">
+                  {NAV__LINKS.map((item, index) => (
+                    <li
+                      onClick={handleNavbar}
+                      key={index}
+                      className="px-8 text-off-white my-3"
+                    >
+                      <Link href={`${item.path}`}>
+                        {translate ? item.spanish : item.display}
+                      </Link>
+                    </li>
+                  ))}
+                  <div
+                    onClick={() => {
+                      setNavbarOpen(!navbarOpen);
+                      setTranslate(!translate);
+                    }}
+                    className="my-3 py-2"
+                  >
+                    {translate ? "english" : "español"}
+                  </div>
+                </ul>
+              </div>
+
+              <div className="flex justify-center my-8 text-3xl text-off-white">
+                <GrInstagram className="mx-2" />
+                <GrFacebookOption className="mx-2" />
+              </div>
+            </section>
+          </motion.div>
+        )}
+      </AnimatePresence>
       {/* ---------------End of Mobile Menu */}
 
       {/* ---------------Start of Desktop Menu */}
